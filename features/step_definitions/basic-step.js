f35e92ebb756e8ad73ed27097f7dfd6d7329de7b
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2018-09-04 15:18:17 +0200

    Set up testing framework Cucumber and add tests

---
+++ features/step_definitions/basic-step.js
@@ -0,0 +1,33 @@
+const { After, Given, Then, When } = require('cucumber')
+
+After(async function() {
+  return await this.closeHomePage()
+})
+
+Given('I visit the site', async function() {
+    return await this.openHomePage()
+})
+
+Then('I should see {string}', async function(content) {
+  return await this.pageHasTextContent(content)
+})
+
+When('I click {string}', async function(string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending'
+})
+
+Then('I fill in {string} with {string}', async function(string, string2) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending'
+})
+
+Then('I should have {int} contact in my address book', async function(int) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending'
+})
+
+Then('I should not see {string}', async function(string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending'
+})
